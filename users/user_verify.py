05c6894425080f6da9f6e8024e0c855a89be3d36
wsh <wsh@wsh.local>
2017-06-02 00:17:04 +0800

    update

--- users/user_verify.py
+++ users/user_verify.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect
 from django.http import HttpResponseRedirect
 
 
-# 在一些需要登录的模块 验证用户是否已经登录，如果已经登录，返回登录信息, 如果没有登录，则跳到登录页面(首页除外)
+# 在一些需要登录的模块 验证用户是否已经登录，如果已经登录，返回登录信息, 如果没有登录，则跳到登录页面
 def user_verify(target_func):
     def wrapper(request):
         # 根据用户发来的session-id，查找服务器本地该session-id对应的session信息中有没有user_id字段
