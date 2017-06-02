32af71f6d2ab9176b0090a16b3da2327e2183dd6
wsh <wsh@wsh.local>
2017-06-02 19:18:58 +0800

    update

--- ttsx/urls.py
+++ ttsx/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('dailyfresh.urls', namespace='dailyfresh')),
+    url(r'^$', include('goods.urls', namespace='dailyfresh')),
     url(r'^', include('users.urls', namespace='users')),
 ]
