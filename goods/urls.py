32ea7f9bb8d9889f8a011592d8514188ee19f351
wsh <wsh@wsh.local>
2017-06-03 17:01:48 +0800

    index complete

--- goods/urls.py
+++ goods/urls.py
@@ -5,7 +5,6 @@ import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^index2/$', views.index2, name='index2$'),
     url(r'^goods_list', views.goods_list, name='goods_list'),
     url(r'^^goods_detail', views.goods_detail, name='goods_detail'),
 ]
--- goods/views.py
+++ goods/views.py
@@ -7,46 +7,25 @@ from django.db.models import Count, Max
 
 
 def index(request):
-    type1_click = GoodsInfo.objects.filter(gtype_id=1).order_by('-gclick')[0:3]
-    type1_new = GoodsInfo.objects.filter(gtype_id=1).order_by('-id')[0:4]
-    type2_click = GoodsInfo.objects.filter(gtype_id=2).order_by('-gclick')[0:3]
-    type2_new = GoodsInfo.objects.filter(gtype_id=2).order_by('-id')[0:4]
-    type3_click = GoodsInfo.objects.filter(gtype_id=3).order_by('-gclick')[0:3]
-    type3_new = GoodsInfo.objects.filter(gtype_id=3).order_by('-id')[0:4]
-    type4_click = GoodsInfo.objects.filter(gtype_id=4).order_by('-gclick')[0:3]
-    type4_new = GoodsInfo.objects.filter(gtype_id=4).order_by('-id')[0:4]
-    type5_click = GoodsInfo.objects.filter(gtype_id=5).order_by('-gclick')[0:3]
-    type5_new = GoodsInfo.objects.filter(gtype_id=5).order_by('-id')[0:4]
-    type6_click = GoodsInfo.objects.filter(gtype_id=6).order_by('-gclick')[0:3]
-    type6_new = GoodsInfo.objects.filter(gtype_id=6).order_by('-id')[0:4]
+    list_model = []
+    type_count = TypeInfo.objects.aggregate(Count('id'))['id__count']
 
-    context = {'title': '首页',
-               'type1_click': type1_click,
-               'type1_new': type1_new,
-               'type2_click': type2_click,
-               'type2_new': type2_new,
-               'type3_click': type3_click,
-               'type3_new': type3_new,
-               'type4_click': type4_click,
-               'type4_new': type4_new,
-               'type5_click': type5_click,
-               'type5_new': type5_new,
-               'type6_click': type6_click,
-               'type6_new': type6_new,
-               }
-    return render(request, 'goods/index.html', context)
-
-
-def index2(request):
-    click_new_list = []
-    type_count = GoodsInfo.objects.aggregate(Max('gtype_id'))['gtype_id__max']
     for i in range(1, type_count+1):
-        click = GoodsInfo.objects.filter(gtype_id=i).order_by('-gclick')[0:3]
-        new = GoodsInfo.objects.filter(gtype_id=i).order_by('-id')[0:4]
-        type_name = TypeInfo.objects.get(id=i)
-        click_new_list.append([type_name, click, new])
+        try:
+            click = GoodsInfo.objects.filter(gtype_id=i).order_by('-gclick')[0:3]
+            new = GoodsInfo.objects.filter(gtype_id=i).order_by('-id')[0:4]
+            type_name = TypeInfo.objects.get(id=i)
+        except:
+            continue
 
-    context = {'click_new_list': click_new_list}
+        banner_id = '0' + str(i) if i < 10 else str(i)
+        list_model.append({'type_name': type_name,
+                           'click': click,
+                           'new': new,
+                           'banner_id': banner_id,
+                           })
+
+    context = {'title': '首页', 'list_model': list_model}
     return render(request, 'goods/index.html', context)
 
 
