a75caeb09c63d2a51fa71256579b0460a3b4ce83
wsh <wsh@wsh.local>
2017-06-08 20:02:53 +0800

    fix bug

--- static/js/register.js
+++ static/js/register.js
@@ -48,7 +48,7 @@ $(function(){
 		}
 		else
 		{
-			$.get('/register_exist/?uname='+$('#user_name').val(), function(data){
+			$.get('/users/register_exist/?uname='+$('#user_name').val(), function(data){
 				if(data.count == 1){
 					$('#user_name').next().html('用户名已存在').show();
 					error_name = true;
